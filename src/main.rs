a4575488146759a3545398b108adb000b2117a3f
Peter Mandile <peterm1994@hotmail.com>
2024-07-05 17:55:19 +1000

    moved serial polling to a scheduled interrupt
    that was way harder than it should have been
    but we can log to serial now which is cool

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+use core::fmt;
+
 use bsp::entry;
 use bsp::hal::{
     clocks::{Clock, init_clocks_and_plls},
@@ -8,6 +11,8 @@ use bsp::hal::{
     sio::Sio,
     watchdog::Watchdog,
 };
+use cortex_m::interrupt::Mutex;
+use cortex_m::peripheral::NVIC;
 use defmt::*;
 #[allow(unused_imports)]
 use defmt::*;
@@ -23,21 +28,24 @@ use embedded_graphics::text::Text;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::spi::MODE_0;
 use fire::Fire;
-use fugit::RateExtU32;
+use fugit::{MicrosDurationU32, RateExtU32};
 use mipidsi::{Builder, Orientation};
 use numtoa::NumToA;
 #[allow(unused_imports)]
 use panic_probe as _;
-use usb_device::class_prelude::UsbBusAllocator;
-use usb_device::prelude::{StringDescriptors, UsbDeviceBuilder, UsbVidPid};
+use usb_device::class_prelude::*;
+use usb_device::prelude::*;
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 use usbd_serial::embedded_io::Write;
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use waveshare_rp2040_zero as bsp;
-use waveshare_rp2040_zero::{Gp0Spi0Rx, Gp1Spi0Csn, Gp2Spi0Sck, Gp3Spi0Tx};
+use waveshare_rp2040_zero::{Gp0Spi0Rx, Gp1Spi0Csn, Gp2Spi0Sck, Gp3Spi0Tx, XOSC_CRYSTAL_FREQ};
 use waveshare_rp2040_zero::hal::Timer;
+use waveshare_rp2040_zero::hal::timer::{Alarm, Alarm0};
 use waveshare_rp2040_zero::hal::usb::UsbBus;
+use waveshare_rp2040_zero::pac::interrupt;
+use waveshare_rp2040_zero::pac::Interrupt::TIMER_IRQ_0;
 
 use frugger_core::{ButtonInput, FruggerGame, FrugInputs};
 
@@ -45,20 +53,31 @@ use crate::mc_inputs::McInputs;
 
 mod mc_inputs;
 
+static mut USB_DEVICE: Option<UsbDevice<UsbBus>> = None;
+static mut USB_BUS: Option<UsbBusAllocator<UsbBus>> = None;
+static mut USB_SERIAL: Mutex<RefCell<Option<SerialPort<UsbBus>>>> = Mutex::new(RefCell::new(None));
+
+static mut ALARM_0: Option<Mutex<RefCell<Alarm0>>> = None;
+
+macro_rules! log {
+    ($($tts:tt)*) => {
+        log_fmt(format_args!($($tts)*))
+    }
+}
+
+
 #[entry]
 fn main() -> ! {
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
+    let mut core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
 
 
     let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+        XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -70,8 +89,12 @@ fn main() -> ! {
         .unwrap();
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+    let mut alarm_0 = timer.alarm_0().unwrap();
+    alarm_0.schedule(MicrosDurationU32::millis(8)).unwrap();
+    alarm_0.enable_interrupt();
 
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+    unsafe { ALARM_0 = Some(Mutex::new(RefCell::new(alarm_0))); }
 
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
@@ -80,18 +103,33 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
+    unsafe {
+        // Enable the interrupt.
+        NVIC::unmask(TIMER_IRQ_0);
+    }
+
     let usb_bus = UsbBusAllocator::new(UsbBus::new(pac.USBCTRL_REGS,
                                                    pac.USBCTRL_DPRAM, clocks.usb_clock, true, &mut pac.RESETS));
+    unsafe {
+        USB_BUS = Some(usb_bus);
+    }
+    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
 
-    let mut serial = SerialPort::new(&usb_bus);
+    let serial = SerialPort::new(&bus_ref);
+    unsafe {
+        USB_SERIAL = Mutex::new(RefCell::new(Some(serial)));
+    }
 
-    let mut usb_device = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x2E8A, 0x000A))
+    let usb_device = UsbDeviceBuilder::new(&bus_ref, UsbVidPid(0x2E8A, 0x000A))
         .strings(&[StringDescriptors::default()
             .manufacturer("Frugger")
             .product("Serial Port")
             .serial_number("TEST")]).unwrap()
         .device_class(USB_CLASS_CDC)
         .build();
+    unsafe {
+        USB_DEVICE = Some(usb_device);
+    }
 
     let mut led_pin = pins.gp5.into_push_pull_output();
 
@@ -168,22 +206,26 @@ fn main() -> ! {
         let text = Text::new(frame_time, Point::new_equal(30), txt_style);
         text.draw(&mut display);
 
-        log("hello", &mut serial);
-        usb_device.poll(&mut [&mut serial]);
+        // log("hello");
+        // log_fmt(format_args!("{}", frame_time));
+        log!("{frame_time}");
 
-        // if frame_elapsed < FRAME_TIME {
-        //     delay.delay_ms((FRAME_TIME - frame_elapsed) as u32);
-        // }
+        if frame_elapsed < FRAME_TIME {
+            delay.delay_ms((FRAME_TIME - frame_elapsed) as u32);
+        }
     }
 }
+fn log_fmt(fmt: fmt::Arguments<'_>) {
+    unsafe {
+        cortex_m::interrupt::free(|cs| {
+            let serial = USB_SERIAL.borrow(&cs);
+            let mut s2 = serial.borrow_mut();
+            let s2 = s2.as_mut().unwrap();
 
-fn log(msg: &str, serial: &mut SerialPort<UsbBus>) {
-    let _ = serial.write(msg.as_bytes());
-    let _ = serial.write("\r\n".as_bytes());
-}
-
-fn log_fmt(msg:&str, serial: &mut SerialPort<UsbBus>) {
-    serial.write_fmt(format_args!("{}", msg));
+            let _ = s2.write_fmt(fmt);
+            let _ = s2.write("\r\n".as_bytes());
+        });
+    }
 }
 
 pub struct RollingAverage {
@@ -210,4 +252,20 @@ impl RollingAverage {
     pub fn average(&self) -> u64 {
         self.sum / 10
     }
+}
+
+#[allow(non_snake_case)]
+#[interrupt]
+unsafe fn TIMER_IRQ_0() {
+    cortex_m::interrupt::free(|cs| {
+        let usb_dev = USB_DEVICE.as_mut().unwrap();
+        let serial = USB_SERIAL.borrow(&cs);
+        let mut s2 = serial.borrow_mut();
+        let s2 = s2.as_mut().unwrap();
+        usb_dev.poll(&mut [s2]);
+
+        let mut alarm = ALARM_0.as_mut().unwrap().borrow(&cs).borrow_mut();
+        alarm.clear_interrupt();
+        alarm.schedule(MicrosDurationU32::millis(8)).unwrap();
+    });
 }
